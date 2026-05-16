068b509e292f33793df555395b7e369617e0ab83
SAMEER KUMAR NAYAK <143753418+samerr592004@users.noreply.github.com>
2026-05-16 11:37:24 +0530

    Created User service and dockerfile.

---
+++ user-service/index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 3001
+
+app.use(bodyParser.json())
+
+mongoose.connect('mongodb://localhost:27017/users').then(() => {
+  console.log('Connected to MongoDB')
+}).catch((err) => {
+  console.error('Error connecting to MongoDB', err)
+})
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+})
+
+const User = mongoose.model('User', userSchema)
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find()
+    res.json(users)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+app.post('/users', async (req, res) => {
+  try {
+    const {name, email} = req.body
+    const user = new User({name, email})
+    await user.save()
+    res.status(201).json(user)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
