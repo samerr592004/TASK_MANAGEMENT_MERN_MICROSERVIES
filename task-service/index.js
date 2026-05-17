138749b522acb3de3d8d5b2124b1f6de7e419812
SAMEER KUMAR NAYAK <143753418+samerr592004@users.noreply.github.com>
2026-05-17 22:07:56 +0530

    Added message functionality to Task service.

--- task-service/index.js
+++ task-service/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const amqp = require('amqplib')
 
 const app = express()
 const port = 3002
+
 
 app.use(bodyParser.json())
 
@@ -22,6 +24,26 @@ const taskSchema = new mongoose.Schema({
 
 const Task = mongoose.model('Task', taskSchema)
 
+let connection,channel;
+async function connectionWIthRabbotMQRetry(retries = 5, delay = 5000) {
+  while(retries){
+  try{
+
+    
+      connection = await amqp.connect('amqp://rabbitmq:')
+      channel = await connection.createChannel()
+      await channel.assertQueue('task_created')
+      console.log('Connected to RabbitMQ')
+      return
+    
+  }catch(error){
+    console.error('Error connecting to RabbitMQ', error)
+    retries--
+    await new Promise(res => setTimeout(res, delay))
+  }
+}
+}
+
 app.get('/tasks', async (req, res) => {
   try {
     const tasks = await Task.find()
@@ -36,6 +58,17 @@ app.post('/tasks', async (req, res) => {
     const {title, description, userId} = req.body
     const task = new Task({title, description, userId})
     await task.save()
+
+    const message = {taskId: task._id, userId,title}
+
+    if(!channel){
+      res.status(503).json({ error: 'RabbitMQ not available' })
+    }
+
+    channel.sendToQueue('task_created', Buffer.from(JSON.stringify(message)))
+
+
+
     res.status(201).json(task)
   } catch (error) {
     console.error('Error creating task:', error)
@@ -51,4 +84,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Task service listening on port ${port}`)
+  connectionWIthRabbotMQRetry()
 })
